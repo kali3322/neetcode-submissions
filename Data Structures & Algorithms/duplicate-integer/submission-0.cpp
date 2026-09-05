b49519ceb66c7051f4a74b4b5d118c4075913b35
Mohamed El Allam <elallammohamed180@gmail.com>
2026-09-06 00:09:50 +0200

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i <  n ; ++i){
+            for(int j = i+1; j < n ; ++j){
+                if(nums.at(j) == nums.at(i)) return true; 
+            }
+        }
+
+        return false; 
+    }
+};

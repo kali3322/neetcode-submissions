4fa5f46460d38c3994d8d9f0449fe43e1a434e34
Mohamed El Allam <elallammohamed180@gmail.com>
2026-09-06 04:03:26 +0200

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t){
+
+        if(s.length() != t.length()) return false; 
+        else if ( s == t) return true; 
+        
+        long  double suma_chars_S = 0.0; 
+        long  double suma_chars_T = 0.0; 
+
+        for(int i = 0 ; i < s.length() ; ++i){
+
+            suma_chars_S+= 1.0/(int)s.at(i); 
+            suma_chars_T+= 1.0/(int)t.at(i); 
+            
+        }
+
+        return abs(suma_chars_S - suma_chars_T) < 1e-9;  
+
+    }
+};

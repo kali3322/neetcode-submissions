af686f236f6a80d118810a149e4538473993fc9c
Mohamed El Allam <elallammohamed180@gmail.com>
2026-09-11 18:36:23 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,38 @@
+#include <cctype>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+    string limpiar_string(string s){
+        string auxiliar=""; 
+        for(int i = 0; i < s.size(); ++i){
+            if(isalnum(s.at(i))){
+                auxiliar+=s.at(i); 
+            }
+        }
+
+        return auxiliar; 
+    }
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string ss = limpiar_string(s); 
+        int n = ss.size(); 
+        int i = 0; 
+        int j = n-1; 
+ 
+        while( i < j ){
+
+            char r1 = tolower(ss.at(i)); 
+            char r2 = tolower(ss.at(j));
+            if(  r1 != r2 ) return false;
+            ++i; 
+            --j; 
+        }
+
+        return true; 
+    }
+};
